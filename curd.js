e21e425c1ce6ab17073a7acff4a73ebde72bf634
YixingShao <registershao99@gmail.com>
2019-06-05 11:27:48 -0700

    routing change, sum update, get batch change

--- curd.js
+++ curd.js
@@ -12,38 +12,37 @@ app.get('/', (req, res) => {
 });
 
 // [START update]
-app.post('/update/:user/:day/:hour/:step/', async (req, res, next) => {
+app.post('/:user/:day/:hour/:step/', async (req, res, next) => {
     const userID = req.params.user;
     const day = req.params.day;
     const hour = req.params.hour;
     const step = req.params.step;
-    console.log("user: "+userID);
-    console.log("day: "+day);
-    console.log("hour: "+hour);
-    console.log("step: "+step);
+    //console.log("user: "+userID);
+    //console.log("day: "+day);
+    //console.log("hour: "+hour);
+    //console.log("step: "+step);
     const stepI = parseInt(step);
     const hourI = parseInt(hour);
     const dayI = parseInt(day);
     if (dayI < 0 || hourI < 0 || stepI < 0 || hourI > 23 || stepI > 5000) {
-        res.write('invalid number');
+        res.send('invalid number');
         res.status(400);
         return
     }
     const update_record_key = ds.key(['UpdateRecord', userID]);
     const recordUpdate = await ds.get(update_record_key);
-    const unique_key = userID + day;
+    const unique_key = userID + "#" +  day;
     const step_record_key = ds.key(['StepRecord', unique_key]);
     const recordStep = await ds.get(step_record_key);
-    console.log(recordUpdate);
-    console.log(recordStep);
+    //console.log(recordUpdate);
+    //console.log(recordStep);
     if(recordUpdate[0] === undefined && recordStep[0] === undefined){
-        //console.log("hello")
         const step_record = {
             step: new Array(24).fill(0),
-            step_sum: new Array(24).fill(0)
+            step_sum: 0
         }
         step_record.step[hourI] = stepI;
-        step_record.step_sum[hourI] = stepI;
+        step_record.step_sum = stepI;
         const update_record = {
             mostRecentDay:dayI,
         }
@@ -62,19 +61,18 @@ app.post('/update/:user/:day/:hour/:step/', async (req, res, next) => {
     else{
         const entities = [];
         //console.log(records[0]);
-        console.log(recordUpdate);
+        //console.log(recordUpdate);
         if(recordUpdate[0].mostRecentDay<dayI){
             recordUpdate[0].mostRecentDay = dayI;
             entities.push(recordUpdate[0]);
         }
         if(recordStep[0] === undefined){
-            console.log('b');
             const step_record = {
                 step: new Array(24).fill(0),
-                step_sum:new Array(24).fill(0),
+                step_sum: 0
             }
             step_record.step[hourI] = stepI;
-            step_record.step_sum[hourI] = stepI;
+            step_record.step_sum = stepI;
             const step_record_entity = {
                 key:step_record_key,
                 data:step_record,
@@ -83,114 +81,92 @@ app.post('/update/:user/:day/:hour/:step/', async (req, res, next) => {
         }
         else{
             //console.log(records[1].step[hourI]);
-            console.log("a");
-            console.log(recordStep[0].step);
+            //console.log(recordStep[0].step);
             if(parseInt(recordStep[0].step[hourI])!=stepI) {
+                console.log(recordStep[0]);
+                recordStep[0].step_sum = recordStep[0].step_sum + stepI - recordStep[0].step[hourI];
+                console.log(recordStep[0].step_sum);
                 recordStep[0].step[hourI] = stepI;
-                if(hourI===0){
-                    recordStep[0].step_sum[hourI] = stepI;
-                }
-                else{
-                    recordStep[0].step_sum[hourI] = stepI+recordStep[0].step_sum[hourI-1];
-                }
                 entities.push(recordStep[0]);
             }
         };
-        console.log(entities)
+        //console.log(entities);
         await ds.upsert(entities);
     };
-    res.write('succeeded');
-    return
+    res.send('succeeded');
 });
 // [End update]
 
 // [Start Current Day Handler]
 app.get('/current/:user',async (req, res, next)=>{
     const userID = req.params.user;
-    console.log(userID);
+    //console.log(userID);
     const update_record_key = ds.key(['UpdateRecord', userID]);
     const recordUpdate = await ds.get(update_record_key);
     if(recordUpdate[0]===undefined){
-        res.write("user "+userID+" not found");
+        res.send("user "+userID+" not found");
         return
     }
     const dayI = recordUpdate[0].mostRecentDay;
-    const step_record_key = ds.key(['StepRecord',userID+dayI.toString()]);
+    const step_record_key = ds.key(['StepRecord',userID+"#"+dayI.toString()]);
     const stepRecord = await ds.get(step_record_key);
-    var sum = 0;
-    for(let i in stepRecord[0].step){
-        sum += stepRecord[0].step[i];
-    }
-    console.log(sum)
-    res.write('Total step count on day %d. for %s is %d.',dayI,userID,sum);
-    return
-
-})
+    const step_sum = stepRecord[0].step_sum;
+    res.send("Total step count on day " + dayI + " for " + userID + " is " + step_sum);
+});
 // [End Current Day Handler]
 
 // [Start Single Day Handler]
 app.get('/single/:user/:day',async (req, res, next)=>{
     const userID = req.params.user;
     const day = req.params.day;
-    const unique_key = userID + day;
+    const unique_key = userID + "#" +  day;
     const step_record_key = ds.key(['StepRecord', unique_key]);
     const stepRecord = await ds.get(step_record_key);
-    var sum = 0;
-    for(let i in stepRecord[0].step){
-        sum += stepRecord[0].step[i];
+    const step_sum = stepRecord[0].step_sum;
+    if (stepRecord === undefined) {
+        if (ds.key(['UpdateRecord', userID]) === undefined) {
+            res.send("user" + userID + "not found");
+        } else {
+            res.send("no data for day"+ day + "for" + userID);
+        }
     }
-    console.log(sum)
-    res.write('Total step count on day %s for %s is %d.',day,userID,sum);
-    return
-})
-// [End Single Day Handler]
-
-// [Start Single Day Handler]
-app.get('/single/:user/:day',async (req, res, next)=>{
-    const userID = req.params.user;
-    const day = req.params.day;
-    const unique_key = userID + day;
-    const step_record_key = ds.key(['StepRecord', unique_key]);
-    const stepRecord = await ds.get(step_record_key);
-    var sum = 0;
-    for(let i in stepRecord[0].step){
-        sum += stepRecord[0].step[i];
-    }
-    console.log(sum)
-    res.write('Total step count on day %s for %s is %d.',day,userID,sum);
-    return
-})
+    res.send("Total step count on day" + day + "for" + userID + "is" + step_sum);
+});
 // [End Single Day Handler]
 
 // [Start Range Day Handler]
-app.get('/range/:startDay/:numDays',async (req, res, next)=>{
+app.get('/range/:user/:startDay/:numDays',async (req, res, next)=>{
     const userID = req.params.user;
     const update_record_key = ds.key(['UpdateRecord',userID]);
     const updateRecord = await ds.get(update_record_key);
     if(updateRecord[0] === undefined){
-        res.write("user "+userID+" not found")
-        return
+        res.send("user "+userID+" not found")
     }
     const startDayI = parseInt(req.params.startDay);
     const numDaysI =parseInt(req.params.numDays);
     const mostRecentDay = updateRecord[0].mostRecentDay;
     if(startDayI>mostRecentDay){
-        res.write('start day is larger than most recent day')
+        res.send('start day is larger than most recent day')
     }
-    const max_day = Math.max(mostRecentDay,startDayI+numDaysI-1)
-    var batch  = []
-    for(let i=startDayI; i<=max_day;i++){
-        const unique_key = ds.key(["StepRecord",userID+i.toString()]);
+    const max_day = Math.min(mostRecentDay+1, startDayI+numDaysI);
+
+    var batch  = new Object();
+
+    for(let i=startDayI; i<max_day; i++){
+        const unique_key = ds.key(["StepRecord",userID+ "#" + i.toString()]);
         const stepRecord = await ds.get(unique_key);
-        batch.push(stepRecord[0]);
-        console.log(stepRecord[0]);
+        if (stepRecord[0] === undefined) {
+            batch[i] = 0;
+        } else {
+            batch[i] = stepRecord[0].step_sum;
+        }
     }
-    return
+    res.send(JSON.stringify(batch));
 });
 // [End Range Day Handler]
 
 // [Start emptying the database]
-app.post('/delete', async (req, res, next) => {
+app.delete('/delete', async (req, res, next) => {
     const queryStepRecord = ds.createQuery("StepRecord");
     //console.log(query);
     const [stepRecords] = await ds.runQuery(queryStepRecord);
@@ -199,7 +175,6 @@ app.post('/delete', async (req, res, next) => {
     const batch = [];
     //console.log(entities);
 
-    //const a = [4,5,6];
     for(let i in stepRecords){
         //console.log(entities[i]);
         batch.push(stepRecords[i][ds.KEY]);
@@ -210,6 +185,7 @@ app.post('/delete', async (req, res, next) => {
     }
     //console.log(batch);
     await ds.delete(batch);
+    res.send("delete succeed");
 })
 // [End emptying the database]
 if (module === require.main) {
